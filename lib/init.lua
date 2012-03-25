75bd1af665cdc6f17ff0998d74d0edff0e3ce997
sd <sd@fucksheep.org>
2012-03-25 04:18:49 +0200

    Add various stolen libraries.
    Signed-off-by: sd <sd@fucksheep.org>

--- lib/init.lua
+++ lib/init.lua
@@ -1,9 +1,18 @@
 -- Bootstrap.
 local stringmeta=getmetatable("")
 stringmeta.__mod = stringmeta.format
+local old_loadfile = loadfile
 
-function loadfile()
+function loadstring(str,chunk)
 end
 
-function loadstring()
+function loadfile(fn)
+	if fn:sub(-5) == '.moon' then
+		local code, err = parse_moon(io.open(fn,"r"):read("*a"))
+		if code then
+			return loadstring(code, fn)
+		end
+		return code, err
+	end
+	return old_loadfile(fn)
 end
